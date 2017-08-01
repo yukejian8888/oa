3f7645047354d8ec8f9216ecda5b47a12fa30aa8
yukejian8888 <yukejian8888@163.com>
2017-08-01 14:26:00 +0800

    delete

--- Home/Model/RoleModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Model;
-use Think\Model;
-class RoleModel extends Model{
-    protected $pk = 'role_id';
-    protected $fields = array(
-        'role_id','role_name','role_nodeid','role_path'
-    );
-}
--- Tools/Test.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Tools; //以目录名称作为命名空间名
-class Test{
-    function getName(){
-        echo "Test-->getName()";
-    }
-}
